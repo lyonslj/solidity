c9d47ed88d1f9913b764069e215f8bb0ed169a53
jl <lyonslj@gmail.com>
2020-11-11 07:50:47 +0200

    Set mesage in deploy script and output to console

--- Solidity201/TruffleDeploy/migrations/2_helloworld_deploy.js
+++ Solidity201/TruffleDeploy/migrations/2_helloworld_deploy.js
@@ -1,5 +1,11 @@
 const Helloworld = artifacts.require("Helloworld");
 
 module.exports = function (deployer) {
-  deployer.deploy(Helloworld);
+  deployer.deploy(Helloworld).then(function(instance){
+      instance.setMessage("Message updated from deploy script").then(function(){
+          instance.getMessage().then(function(message){
+              console.log("Current message: " + message);
+          })
+      });
+  });
 };
