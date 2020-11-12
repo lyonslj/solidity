eaccce8e5ef45a967c5a673f1568723c9e77d8a6
jl <lyonslj@gmail.com>
2020-11-12 07:50:51 +0200

    add second unit test

--- Solidity201/TruffleDeploy/test/helloworldtest.js
+++ Solidity201/TruffleDeploy/test/helloworldtest.js
@@ -4,7 +4,15 @@ contract("Helloworld", async function(){                        //contract Hello
   it("should initialize correctly", async function(){
     let instance = await Helloworld.deployed();
     let message = await instance.getMessage();
-    assert(message === "Message updated from deploy script"     //message should get updated to what you have specified, note ===
-    , "Message should be: Message updated from deploy script");                        //js assert similar but not same as solidity
-  });
+    assert(message === "Message updated from deploy script.."     //message should get updated to what you have specified, note ===
+    , "Message should be: Message updated from deploy script..");                        //js assert similar but not same as solidity
+    }); 
+    // add second test case
+    it("should set the message correctly", async function(){    //description of test 
+        let instance = await Helloworld.deployed();
+        await instance.setMessage("Testing Message");
+        let message = await instance.getMessage();
+        assert(message === "Testing Message"
+        , "Message should be the same as we set it to"); 
+    });
 });
