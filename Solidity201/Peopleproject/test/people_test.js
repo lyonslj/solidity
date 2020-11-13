2536707d9f50542f89a76a9edc263fa3f1346238
jl <lyonslj@gmail.com>
2020-11-13 07:19:27 +0200

    Initial test using truffle-assertions which are -ve tests

---
+++ Solidity201/Peopleproject/test/people_test.js
@@ -0,0 +1,10 @@
+const People = artifacts.require("People");
+const truffleAssert = require("truffle-assertions");
+
+contract("People", async function(){
+  it("shouldn't create a person with age over 150 years", async function(){
+    let instance = await People.deployed();
+    // this actually passes the test in truffle ie convers of normal test because age ie 200 > 150
+    await truffleAssert.fails(instance.createPerson("Bob", 200, 190, {value: web3.utils.toWei("1", "ether")}))
+    })
+});
