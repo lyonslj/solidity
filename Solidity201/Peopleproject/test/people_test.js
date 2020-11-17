25dace905b4905560c5f56f70c0491ada7280621
jl <lyonslj@gmail.com>
2020-11-17 06:56:33 +0200

    truffleAssert  positive and negative test

--- Solidity201/Peopleproject/test/people_test.js
+++ Solidity201/Peopleproject/test/people_test.js
@@ -2,10 +2,10 @@ const People = artifacts.require("People");
 const AssertionError = require("assertion-error");
 const truffleAssert = require("truffle-assertions");
 
-contract("People", async function(){
+contract("People", async function(accounts){
   it("shouldn't create a person with age over 150 years", async function(){
     let instance = await People.deployed();
-    // this actually passes the test in truffle ie convers of normal test because age ie 200 > 150
+    // this actually passes the test in truffle ie converse of normal test because age ie 200 > 150
     await truffleAssert.fails(instance.createPerson("Bob", 200, 190, {value: web3.utils.toWei("1", "ether")}),
     // Force the contract to REVERT
     truffleAssert.ErrorType.REVERT);
@@ -22,5 +22,20 @@ contract("People", async function(){
         await instance.createPerson("Bob", 65, 190, {value: web3.utils.toWei("1", "ether")});
         let result = await instance.getPerson();
         assert(result.senior === true, "Senior level not set");
+    });
+    // Test that only the creator of a person can delete that person - negative test
+    // account[0] will always be creator
+    it("test if any account other than owner can delete person ", async function(){
+        let instance = await People.deployed();
+        //Create with accounts[1]
+        await instance.createPerson("Bob", 50, 190, {from: accounts[1],value: web3.utils.toWei("1", "ether")});
+        //Test for failure deleltePerson with accounts[1]
+        await truffleAssert.fails(instance.deletePerson(accounts[1], {from: accounts[1]}), truffleAssert.ErrorType.REVERT);
+    })
+    // Now do the positive test to deletePerson with Creator
+    it("test that only the creator of a person can delete that person ", async function(){
+        let instance = await People.deployed();
+        await instance.createPerson("Graeme", 50, 190, {from: accounts[0],value: web3.utils.toWei("1", "ether")});
+        await truffleAssert.passes(instance.deletePerson(accounts[1], {from: accounts[0]}), truffleAssert.ErrorType.REVERT);
     })
 });
