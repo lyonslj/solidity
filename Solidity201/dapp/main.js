6e6aafad64ffa4b3c5f03578fd2ff03452eeb60f
jl <lyonslj@gmail.com>
2020-11-24 08:24:40 +0200

    Updated main with 1) contract acc 2) abi 3) MM acc

--- Solidity201/dapp/main.js
+++ Solidity201/dapp/main.js
@@ -1,7 +1,9 @@
 var web3 = new Web3(Web3.givenProvider);
 
-
 $(document).ready(function() {
-    window.ethereum.enable()
+    window.ethereum.enable().then(function(accounts){
+        contractInstance = new.web3.eth.Contract(abi,"0x26d1461066Da79f7d5F664FF149C0e6976257D76",{from: accounts[0]});
+        console.log(contractInstance);
+    })
 
 });
