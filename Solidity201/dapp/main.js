fb59a6c99ca723aa8b5293005aae739692c72b5e
jl <lyonslj@gmail.com>
2020-11-25 10:24:19 +0200

    Add function so data button interacts with SC via MM ie add name, age, height

--- Solidity201/dapp/main.js
+++ Solidity201/dapp/main.js
@@ -1,12 +1,12 @@
 var web3 = new Web3(Web3.givenProvider);
+var contractInstance;
 
 $(document).ready(function() {
     window.ethereum.enable().then(function(accounts){
-        contractInstance = new.web3.eth.Contract(abi,"0x26d1461066Da79f7d5F664FF149C0e6976257D76",{from: accounts[0]});
+        contractInstance = new web3.eth.Contract(window.abi, "0x26d1461066Da79f7d5F664FF149C0e6976257D76", {from: accounts[0]});
         console.log(contractInstance);
-    });
-    $("#add_data_button").click(inputData);
-
+        });
+        $("#add_data_button").click(inputData);    
 });
 
 // Create function to input data - name, age, height
